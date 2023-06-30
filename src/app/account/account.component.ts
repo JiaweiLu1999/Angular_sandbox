bf37a0001b90fe18ac3c296e9495461ea21c0739
Jiawei Lu <jl5999@columbia.edu>
2023-06-30 15:45:45 -0400

    service finish

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,20 +1,25 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {LoggingService} from "../logging.service";
+import {AccountService} from "../account.service";
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
 })
 export class AccountComponent {
   // @ts-ignore
   @Input() account: {name: string, status: string};
   // @ts-ignore
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+
+  constructor(private loggingService: LoggingService, private accountService: AccountService) {
+  }
 
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.accountService.changeStatus(this.id, status);
+    this.loggingService.logStatusChange(status);
   }
 }
