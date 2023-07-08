dcd8ae440bfb5615fd714bc648d23108897c3dd6
Jiawei Lu <jl5999@columbia.edu>
2023-07-08 19:02:08 -0400

    template-driven form complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //@ts-ignore
+  @ViewChild('f', {static: true}) signupForm: NgForm;
+
+  defaultOption = "pet";
+  questionAnswer = '';
+  genders = ["male", "female", "other"];
+  submitted = false;
+  user = {
+    username: "",
+    gender: "",
+    email: "",
+    question: "",
+    answer: ""
+  }
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+      "user-data": {
+        username: suggestedName
+      }
+    });
+
+  }
+
+  onSubmit(form: NgForm) {
+    this.submitted = true;
+    this.user.username = form.value["user-data"]["username"];
+    this.user.gender = form.value["user-data"]["gender"];
+    this.user.email = form.value["user-data"]["email"];
+    this.user.question = form.value["secret"];
+    this.user.answer = form.value["questionAnswer"];
+    form.reset();
   }
 }
