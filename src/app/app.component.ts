69b20417648e608b54115c77d55d7c6c25a082c0
Jiawei Lu <jl5999@columbia.edu>
2023-07-12 07:43:43 -0400

    reactive form start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
-import {NgForm} from "@angular/forms";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,38 +6,5 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //@ts-ignore
-  @ViewChild('f', {static: true}) signupForm: NgForm;
-
-  defaultOption = "pet";
-  questionAnswer = '';
-  genders = ["male", "female", "other"];
-  submitted = false;
-  user = {
-    username: "",
-    gender: "",
-    email: "",
-    question: "",
-    answer: ""
-  }
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    this.signupForm.form.patchValue({
-      "user-data": {
-        username: suggestedName
-      }
-    });
-
-  }
-
-  onSubmit(form: NgForm) {
-    this.submitted = true;
-    this.user.username = form.value["user-data"]["username"];
-    this.user.gender = form.value["user-data"]["gender"];
-    this.user.email = form.value["user-data"]["email"];
-    this.user.question = form.value["secret"];
-    this.user.answer = form.value["questionAnswer"];
-    form.reset();
-  }
+  genders = ['male', 'female'];
 }
