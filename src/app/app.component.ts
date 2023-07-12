6b32bbf9fa657fcb99b41fd272dd69ed6dbceb8f
Jiawei Lu <jl5999@columbia.edu>
2023-07-12 15:00:42 -0400

    pipe start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,42 @@
-import {Component, OnInit} from '@angular/core';
-import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  signupForm: FormGroup = new FormGroup({});
-  projectStatus = ['Stable', 'Critical', 'Finished'];
-  forbiddenProjectName = ['Test'];
-  forbiddenEmail = ['jl5999@columbia.edu'];
-
-  ngOnInit(): void {
-    this.signupForm = new FormGroup({
-      "projectName": new FormControl(null, [Validators.required, this.projectNameValidator.bind(this)]),
-      "email": new FormControl(null, [Validators.email, Validators.required], this.emailValidator.bind(this)),
-      "projectStatus": new FormControl('Stable')
-    });
-  }
-
-
-
-  projectNameValidator(control: FormControl): {[s:string]: boolean} | null {
-    if (this.forbiddenProjectName.indexOf(control.value) !== -1) {
-      return {'projectNameForbidden' : true};
+export class AppComponent {
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
     }
-    return null;
+  ];
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
   }
-
-  emailValidator(control: AbstractControl): Promise<any> {
-    return new Promise(
-      (resolve, reject) => {
-        setTimeout(()=> {
-          if (this.forbiddenEmail.indexOf(control.value) !== -1) {
-            resolve({'emailIsForbidden': true});
-          } else {
-            resolve(null);
-          }
-        }, 1000);
-      }
-    );
-  }
-
-  onSubmit() {
-    console.log(this.signupForm.value);
-  }
-
-
 }
